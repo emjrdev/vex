943c953b53bae667f3b932a0b085e0f7a6bbc476
Ernesto Malave <emjrdev@gmail.com>
2019-03-06 15:01:33 -0500

    add DateTime handling to Vex.Blank

--- lib/vex/blank.ex
+++ lib/vex/blank.ex
@@ -36,6 +36,11 @@ defimpl Vex.Blank, for: Map do
   def blank?(map), do: map_size(map) == 0
 end
 
+defimpl Vex.Blank, for: DateTime do
+  def blank?(nil), do: true
+  def blank?(_), do: false
+end
+
 defimpl Vex.Blank, for: NaiveDateTime do
   def blank?(nil), do: true
   def blank?(_), do: false
